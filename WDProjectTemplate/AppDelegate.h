2b6f2b03423872a26eb894a838e641846b8f88e1
yuezaixz <303178394@qq.com>
2015-12-23 22:45:33 +0800

    project init

---
+++ WDProjectTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDProjectTemplate
+//
+//  Created by 吴迪玮 on 15/12/23.
+//  Copyright © 2015年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDProjectTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDProjectTemplate
+//
+//  Created by 吴迪玮 on 15/12/23.
+//  Copyright © 2015年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
